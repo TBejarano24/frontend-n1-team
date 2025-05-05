05401036af629b7c76a0ef8941f78af08367c84f
TBejarano24 <bejaranotomas04@gmail.com>
2025-05-05 17:08:30 -0300

    commit main.js

---
+++ main.js
@@ -0,0 +1,71 @@
+const cursos = [
+  {
+    nombre: "Programación Web",
+    profesor: "Kevin",
+    estudiantes: [
+      { nombre: "Ana", nota: 8 },
+      { nombre: "Luis", nota: 6 },
+      { nombre: "Sofía", nota: 9 },
+      { nombre: "Carlos", nota: 7 },
+    ],
+  },
+  {
+    nombre: "Bases de Datos",
+    profesor: "Diego",
+    estudiantes: [
+      { nombre: "Pedro", nota: 5 },
+      { nombre: "Sofía", nota: 7 },
+      { nombre: "Luis", nota: 4 },
+      { nombre: "Ana", nota: 6 },
+    ],
+  },
+  {
+    nombre: "Diseño UX/UI",
+    profesor: "Lucía",
+    estudiantes: [
+      { nombre: "Ana", nota: 10 },
+      { nombre: "Pedro", nota: 6 },
+      { nombre: "Valentina", nota: 9 },
+      { nombre: "Carlos", nota: 10 },
+    ],
+  },
+  {
+    nombre: "Lógica de Programación",
+    profesor: "Martín",
+    estudiantes: [
+      { nombre: "Luis", nota: 5 },
+      { nombre: "Cristina", nota: 8 },
+      { nombre: "Ricardo", nota: 9 },
+      { nombre: "Sofía", nota: 6 },
+    ],
+  },
+  {
+    nombre: "JavaScript Intermedio",
+    profesor: "Mariana",
+    estudiantes: [
+      { nombre: "Kevin", nota: 7 },
+      { nombre: "Valentina", nota: 8 },
+      { nombre: "Pedro", nota: 9 },
+      { nombre: "Ana", nota: 10 },
+    ],
+  },
+];
+//   FUNCION 1: Listar
+//   Mostrá todos los nombres de los cursos con su profesor.
+
+//   Mostrá todos los nombres de los estudiantes (sin repetir).
+
+//   FUNCION 2: Filtrar
+//   Mostrá solo los cursos donde Sofía está como estudiante.
+
+//   Mostrá todos los cursos que tienen al menos un estudiante con nota mayor a 8.
+
+//   FUNCION 3: Buscar
+//   ¿En qué curso está Luis y cuál es su nota?
+
+//   ¿Qué nota sacó Ana en el curso "Diseño UX/UI"?
+
+//   FUNCION 4: Avanzado
+//   Calculá el promedio de notas del curso "Programación Web".
+
+//   Mostrá por cada curso: nombre del curso y nota promedio de sus estudiantes.
